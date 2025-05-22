14e3e095a25b39321a9ed8dde6ec1c211748445e
Dorianaadc <doriana.ja2025@gmail.com>
2025-05-22 09:13:57 +0200

    entidades y relaciones

---
+++ backend/springboot/src/main/java/com/example/springboot/repository/CategoriaRepository.java
@@ -0,0 +1,7 @@
+package com.example.springboot.repository;
+
+import com.example.springboot.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository <Categoria, Long> {
+}
