d4441dfa47817398d9795f71f95d4a39b4d12f41
Sofía <soceriba@gmail.com>
2025-05-24 12:23:22 +0200

    feat: Home page basic style
    Se ha creado la home page dandole un estilo basico. Luego tambien se ha
    añadido una imagen de stock como fondo que se esperará cambiar luego. Se
    han modificado main.tsx y App.tsx añadiendole funcionalidad basica para
    soportar el home. Tambien se han añadido estilos de css para cosas
    generales, llamados main.css y variables.css. El primero de momento esta
    usado para formatear los estilos por defecto de navegadores y el segundo
    sirve para añadir variables de uso general, de momento solo esta creada
    una variable para el ancho querido para la página

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App.tsx";
+
+import "./css/main.css";
+import "./css/variables.css";
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
--- FrontEnd/src/pages/home/index.tsx
+++ FrontEnd/src/pages/home/index.tsx
@@ -1,12 +1,15 @@
 import Footer from "../../componentes/footer";
 import Header from "../../componentes/header";
 
+import classes from "./home.module.css";
+
 function Home() {
     return (
         <>
             <Header />
 
             <Footer />
+            <div className={classes["background-blur"]}></div>
         </>
     );
 }
