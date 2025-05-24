77973c360866fb66ca04c0341fc73b52e1a2b1cd
Sofía <soceriba@gmail.com>
2025-05-24 13:14:48 +0200

    feat: Footer estilo flex
    Se ha cambiado el footer para que tenga un display flex en vez de como
    estaba previamente.

--- FrontEnd/src/componentes/footer/index.tsx
+++ FrontEnd/src/componentes/footer/index.tsx
@@ -1,14 +1,18 @@
+import classes from "./footer.module.css";
+
 function Footer() {
     return (
-        <footer>
-            <div>Footer</div>
-            <ul>
-                <li>Elemento 1</li>
-                <li>Elemento 2</li>
-                <li>Elemento 3</li>
-                <li>Elemento 4</li>
-                <li>Elemento 5</li>
-            </ul>
+        <footer className={classes["page-footer"]}>
+            <div className={classes["page-footer__container"]}>
+                <h2>Footer</h2>
+                <ul className={classes["page-footer__list"]}>
+                    <li>Elemento 1</li>
+                    <li>Elemento 2</li>
+                    <li>Elemento 3</li>
+                    <li>Elemento 4</li>
+                    <li>Elemento 5</li>
+                </ul>
+            </div>
         </footer>
     );
 }
