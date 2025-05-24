a39bf026334c866979e487cce29b3bc718bece12
Sofía <soceriba@gmail.com>
2025-05-24 12:28:45 +0200

    feat: Header funcionalidad basica
    Se ha creado el header con su respectivo css. Tiene funcionalidades
    basicas aunque requiere de futuras modificaciones. No tiene enlaces
    funcionales y el carrito se deberá implementar en un futuro.
    Modicaciones esperables en el css del header para hacerlo mas vistoso

--- FrontEnd/src/componentes/header/index.tsx
+++ FrontEnd/src/componentes/header/index.tsx
@@ -1,8 +1,16 @@
+import classes from "./header.module.css";
+
 function Header() {
     return (
-        <>
-            <div>Hola</div>
-        </>
+        <header className={classes["page-header"]}>
+            <h1>Mercadillo Local</h1>
+
+            <ul className={classes["page-header__list"]}>
+                <li>¿Quienes somos?</li>
+                <li>Perfil</li>
+                <li className={classes["page-header__cart"]}>Carrito (0)</li>
+            </ul>
+        </header>
     );
 }
 
