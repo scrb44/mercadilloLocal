950e95fc852914a085a225cc0532e43ae89c3430
Sofía <soceriba@gmail.com>
2025-06-02 22:59:42 +0200

    feat: Añadido al barrel los componentes municipios

--- FrontEnd/src/componentes/index.tsx
+++ FrontEnd/src/componentes/index.tsx
@@ -1,4 +1,4 @@
-// src/componentes/index.ts - Barrel de componentes CORREGIDO
+// src/componentes/index.ts - ACTUALIZADO CON MUNICIPIO
 
 // Componentes básicos existentes
 export { default as Header } from "./header";
@@ -12,11 +12,18 @@ export { default as ProductList } from "./productList";
 // Cards existentes
 export { default as ProductCard } from "./productCard";
 
-// Componentes nuevos modularizados (solo incluir si los has creado)
+// Componentes nuevos modularizados
 export { default as CategoryHeader } from "./categoryHeader";
 export { default as ProductsSection } from "./productSection";
 export { default as ProductGallery } from "./productGallery";
 export { default as CartItem } from "./cartItem";
+
+// Componentes de municipio
+export { default as MunicipioIndicator } from "./municipioIndicator";
+
+// Guards y Error Handling
+export { default as MunicipioGuard } from "./municipioGuard";
+export { default as ErrorBoundary, NotFoundPage } from "./errorBoundary";
 
 // Navegación - breadcrumb exporta múltiples componentes
 export {
