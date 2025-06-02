7a37875385395be55a9c71d99d6972ac6f2872f8
Dorianaadc <doriana.ja2025@gmail.com>
2025-06-02 16:38:28 +0200

    fix: token hecho, productos si aparecen

--- Backend/src/main/java/com/example/springboot/security/SecurityConfig.java
+++ Backend/src/main/java/com/example/springboot/security/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/protegida").permitAll()
+                        .requestMatchers("/api/productos/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
